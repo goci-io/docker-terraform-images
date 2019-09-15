87de613c9a1d06a7320445d2cb1d96338a6a0b4d
etwillbefine <tim.gatzemeier@gmx.de>
2019-09-15 13:57:19 +0200

    add kops env vars

--- aws/env.sh
+++ aws/env.sh
@@ -6,6 +6,11 @@ if [[ -z "$TF_BUCKET" ]]; then
     export TF_DYNAMODB_TABLE="${NAMESPACE}-${STAGE}-terraform-state-lock"
 fi
 
+if [[ -z "$KOPS_CLUSTER_NAME" ]]; then
+    export KOPS_STATE_STORE="${NAMESPACE}-${STAGE}-kops-state-${REGION}"
+    export KOPS_CLUSTER_NAME="${STAGE}.${REGION}.${NAMESPACE}"
+fi
+
 if [[ ! -z "$AWS_ASSUME_ROLE_NAME" ]]; then
     export AWS_ASSUME_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ASSUME_ROLE_NAME}"
 fi
