8b2e7060a365a44401bddcb5e4a59cd794972998
etwillbefine <tim.gatzemeier@gmx.de>
2019-12-31 18:28:46 +0100

    install aws-vault to local/bin, only if required

--- aws/local-start.sh
+++ aws/local-start.sh
@@ -7,10 +7,10 @@ export TF_CLI_ARGS_init="${argName}=bucket=${TF_BUCKET} ${argName}=region=${AWS_
 export TF_IN_AUTOMATION=""
 export TF_INPUT=1
 
-curl -L -o ./aws-vault https://github.com/99designs/aws-vault/releases/download/v4.2.0/aws-vault-linux-amd64
-
 if [[ "$AWS_VAULT_ENABLED" != "false" ]]; then
     echo "Installing and setting up aws-vault"
+    sudo curl -L -o /usr/local/bin/aws-vault https://github.com/99designs/aws-vault/releases/download/v4.2.0/aws-vault-linux-amd64
+
     export AWS_PROFILE=${AWS_PROFILE:-"$NAMESPACE-$STAGE"}
     export AWS_DEFAULT_PROFILE=${AWS_PROFILE}
 
@@ -20,7 +20,7 @@ if [[ "$AWS_VAULT_ENABLED" != "false" ]]; then
 
     echo "Spawing bash using aws-vault $AWS_PROFILE"
     echo "Please unlock your stored aws-vault key"
-    ./aws-vault exec ${AWS_PROFILE} -- /bin/bash
+    aws-vault exec ${AWS_PROFILE} -- /bin/bash
 else
     /bin/bash
 fi
